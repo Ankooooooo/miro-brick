b11dd07cf2d186c06d8f86909486f3545771d09d
hyangjee <gidwl1@naver.com>
2024-02-17 19:32:28 +0900

    header, navigation, footer

--- README.md
+++ README.md
@@ -4,8 +4,8 @@
 > 형태: 반응형 웹
 
 ### Device Size Guide
-> Mobile: Width <= 390px  
-> Tablet: 391px < Width <= 834px  
+> Mobile: Width <= 834px  
+> Tablet: 834px < Width <= 1312px  
 > PC: 1312px <= Width
 
 ### Install Extension
--- assets/js/base.js
+++ assets/js/base.js
@@ -1,5 +1,5 @@
 // data-include-path
-window.addEventListener('load', function() {
+function includePath(callback) {
     var allElements = document.getElementsByTagName('*');
     Array.prototype.forEach.call(allElements, function(el) {
         var includePath = el.dataset.includePath;
@@ -8,36 +8,74 @@ window.addEventListener('load', function() {
             xhttp.onreadystatechange = function () {
                 if (this.readyState == 4 && this.status == 200) {
                     el.outerHTML = this.responseText;
+                    callback();
                 }
             };
             xhttp.open('GET', includePath, true);
             xhttp.send();
         }
     });
-});
-
-window.device = 'mobile';
-
-
-
-function findViewType() {
-    let { innerWidth } = window
-    
-    if (innerWidth <= 390) window.device = 'mobile'
-    else if (innerWidth > 390 && innerWidth <= 834) window.device = 'tablet'
-    else window.device = 'window'
-    
-    console.log('innerWidth', innerWidth);
-    console.log('device', window.device);
 }
 
+window.viewport = 'pc';
+function findViewPort() {
+    let { innerWidth } = window
+    
+    if (innerWidth <= 834) window.viewport = 'mobile'
+    else if (innerWidth > 834 && innerWidth <= 1312) window.viewport = 'tablet'
+    else window.viewport = 'pc'
+}
 
-// Mobile 여부 확인
+function showViewport() {
+    var allElements = document.querySelectorAll('[data-viewport]');
+    allElements.forEach(function(el) {
+        var viewport = el.dataset.viewport;
+        if ( viewport && !viewport.includes(window.viewport) ) {
+            el.style.display = 'none';
+        } else {
+            el.style.display = '';
+        }
+    });
+
+    console.log('viewport: ', window.viewport);
+    console.log('innerWidth: ', innerWidth);
+}
+
+// 메뉴별 타이틀 설정
+function setHeaderTitle(title) {
+    const el_header = document.querySelector('header');
+
+    if (el_header.className.includes('sub')) {
+        const el = document.querySelector(".header__title");
+        el.innerText = title;
+    } else {
+        const elements = document.querySelectorAll('.header [label]');
+
+        elements.forEach(function(el) {
+            if(el.getAttribute('label') === title) {
+                el.classList.add('selected');
+            }
+        });
+    }
+}
+
+// Navigation 선택된 메뉴 설정
+function setSelectedNaviation(title) {
+    const elements = document.querySelectorAll('.navigation [label]');
+
+    elements.forEach(function(el) {
+        if(el.getAttribute('label') === title) {
+            el.classList.add('selected');
+        }
+    });
+}
+
 window.addEventListener('load', function() {
-    findViewType();
+    findViewPort();
+    includePath(showViewport);
 });
 
-// ====== 창 리사이즈때마다 현재 기기가 무엇인지 판단
 window.addEventListener('resize', function() {
-    findViewType();
+    findViewPort();
+    showViewport();
 });
