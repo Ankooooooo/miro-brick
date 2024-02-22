06a087c91522229ba708a98e04e17c0fb3c554e5
hyangji <leehj@gsitm.com>
2024-02-22 16:43:32 +0900

    제품 navigation에 dragToScroll 추가

--- assets/js/dragToScroll.js
+++ assets/js/dragToScroll.js
@@ -1,28 +1,27 @@
-// Main 페이지 CareerType, EtcContent에서 사용
 // 마우스 드래그 스크롤 기능 및 가로 스크롤
-export function dragToScroll(selector_name) {
-    const target = document.querySelector(`${selector_name} .drag__area`)
-    if (!target) return
+function dragToScroll(selector_name) {
+    const target = document.querySelector(`${selector_name} .drag__area`);
+    if (!target) return;
 
-    let isDown = false
-    let startX = 0, scrollLeft = 0
+    let isDown = false;
+    let startX = 0, scrollLeft = 0;
 
     // 마우스 드래그
     target.addEventListener('mousedown', (e) => {
-        isDown = true
-        startX = e.pageX - target.offsetLeft
-        scrollLeft = target.scrollLeft
-    })
+        isDown = true;
+        startX = e.pageX - target.offsetLeft;
+        scrollLeft = target.scrollLeft;
+    });
 
-    target.addEventListener('mouseleave', () => isDown = false )
-    target.addEventListener('mouseup', () => isDown = false )
+    target.addEventListener('mouseleave', () => isDown = false);
+    target.addEventListener('mouseup', () => isDown = false);
 
     target.addEventListener('mousemove', (e) => {
-        if (!isDown) return
-        e.preventDefault()
+        if (!isDown) return;
+        e.preventDefault();
 
-        const x = e.pageX - target.offsetLeft
-        const walk = (x - startX) * 1.5 //scroll-fast
-        target.scrollLeft = scrollLeft - walk
-    })
-}
+        const x = e.pageX - target.offsetLeft;
+        const walk = (x - startX) * 1.5; //scroll-fast
+        target.scrollLeft = scrollLeft - walk;
+    });
+};
